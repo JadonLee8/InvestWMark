292b896f7d9272580407cd6da3ab8de0575e38eb
JadonLee8 <jadonlee888@gmail.com>
2022-03-11 21:45:55 -0600

    Pushing one last time today to make sure everything is saved and also bc I added a bunch of test cryptos to the main file and it turns out about 70% work, although most of them are quite popular anyways.

--- Main.py
+++ Main.py
@@ -7,6 +7,27 @@ print(bitcoin.get_data("2020-01-01", "2020-02-01", "1d"))
 ethereum = Crypto("eth", "Ethereum", "binance")
 print(ethereum.get_data("2020-01-01", "2020-02-01", "1d"))
 
+binance = Crypto("bnb", "Binance Blockchain Coin", "binance")
+print(binance.get_data("2020-01-01", "2020-02-01", "1d"))
 
+Terra = Crypto("luna", "Terra", "binance")
+print(Terra.get_data("2020-01-01", "2020-02-01", "1d"))  # none
 
+xrp = Crypto("xrp", "xrp", "binance")
+print(xrp.get_data("2020-01-01", "2020-02-01", "1d"))
 
+cardano = Crypto("ada", "Cardano", "binance")
+print(cardano.get_data("2020-01-01", "2020-02-01", "1d"))
+
+solana = Crypto("sol", "Solana", "binance")
+print(solana.get_data("2020-01-01", "2020-02-01", "1d"))  # either doesn't have data available for selected dates or
+# API doesn't have same with Terra and avalanche
+
+avalanche = Crypto("avax", "Avalanche", "binance")
+print(avalanche.get_data("2020-01-01", "2020-02-01", "1d"))  # none
+
+dogecoin = Crypto("doge", "DogeCoin", "binance")
+print(dogecoin.get_data("2020-01-01", "2020-02-01", "1d"))
+
+monero = Crypto("xmr", "Monero", "binance")
+print(monero.get_data("2020-01-01", "2020-02-01", "1d"))

750b7e4d3bec63f8097105f0fe925eb063e166b4
JadonLee8 <jadonlee888@gmail.com>
2022-03-12 09:18:32 -0600

    Added a new param to the get_data function which allows you to specify the exact data you want (ex: open, low, high, close, volume) which makes it much easier to use. Altered the current calls to these methods in Main to accommodate the changes in ChartData.

--- ChartData.py
+++ ChartData.py
@@ -11,14 +11,36 @@ class Crypto:
     # start_date and end_date should be formatted as YYYY-MM-DD
     # interval can be "1m" "5m" "15m" "30m" "1h" "1d" "1w"
     # all params should be strings
-    def get_data(self, start_date, end_date, interval):
+    def get_data(self, start_date, end_date, interval, value_request):
         print("fetching JSON from messari.io API...")
         url = "https://data.messari.io/api/v1/markets/" + self.pref_source + "-" + self.ticker + "-usdt/metrics/price/time-series?start=" + start_date + "&end=" + end_date + "&interval=" + interval + "&format=json"
         json_file = urllib.request.urlopen(url).read()
         # json_file = json_file[1:]
         print("JSON fetched for " + self.name)
         values = psiphon("values", json_file)
-        return values
+        return_val = []
+        if values is not None:
+            for i in values:
+                temp_day = i
+                for y in temp_day:
+                    if value_request == "opens" and temp_day.index(y) == 1:
+                        return_val.append(y)
+                        break
+                    elif value_request == "highs" and temp_day.index(y) == 2:
+                        return_val.append(y)
+                        break
+                    elif value_request == "lows" and temp_day.index(y) == 3:
+                        return_val.append(y)
+                        break
+                    elif value_request == "closes" and temp_day.index(y) == 4:
+                        return_val.append(y)
+                        break
+                    elif value_request == "volumes" and temp_day.index(y) == 5:
+                        return_val.append(y)
+                        break
+            return return_val
+        else:
+            return None
 
 
 def psiphon(keyword, file):
--- Main.py
+++ Main.py
@@ -2,32 +2,32 @@ from ChartData import Crypto
 import ChartData
 
 bitcoin = Crypto("btc", "Bitcoin", "binance")
-print(bitcoin.get_data("2020-01-01", "2020-02-01", "1d"))
+print(bitcoin.get_data("2020-01-01", "2020-02-01", "1d", "opens"))
 
 ethereum = Crypto("eth", "Ethereum", "binance")
-print(ethereum.get_data("2020-01-01", "2020-02-01", "1d"))
+print(ethereum.get_data("2020-01-01", "2020-02-01", "1d", "opens"))
 
 binance = Crypto("bnb", "Binance Blockchain Coin", "binance")
-print(binance.get_data("2020-01-01", "2020-02-01", "1d"))
+print(binance.get_data("2020-01-01", "2020-02-01", "1d", "opens"))
 
 Terra = Crypto("luna", "Terra", "binance")
-print(Terra.get_data("2020-01-01", "2020-02-01", "1d"))  # none
+print(Terra.get_data("2020-01-01", "2020-02-01", "1d", "opens"))  # none
 
 xrp = Crypto("xrp", "xrp", "binance")
-print(xrp.get_data("2020-01-01", "2020-02-01", "1d"))
+print(xrp.get_data("2020-01-01", "2020-02-01", "1d", "opens"))
 
 cardano = Crypto("ada", "Cardano", "binance")
-print(cardano.get_data("2020-01-01", "2020-02-01", "1d"))
+print(cardano.get_data("2020-01-01", "2020-02-01", "1d", "opens"))
 
 solana = Crypto("sol", "Solana", "binance")
-print(solana.get_data("2020-01-01", "2020-02-01", "1d"))  # either doesn't have data available for selected dates or
+print(solana.get_data("2020-01-01", "2020-02-01", "1d", "opens"))  # either doesn't have data available for selected dates or
 # API doesn't have same with Terra and avalanche
 
 avalanche = Crypto("avax", "Avalanche", "binance")
-print(avalanche.get_data("2020-01-01", "2020-02-01", "1d"))  # none
+print(avalanche.get_data("2020-01-01", "2020-02-01", "1d", "opens"))  # none
 
 dogecoin = Crypto("doge", "DogeCoin", "binance")
-print(dogecoin.get_data("2020-01-01", "2020-02-01", "1d"))
+print(dogecoin.get_data("2020-01-01", "2020-02-01", "1d", "opens"))
 
 monero = Crypto("xmr", "Monero", "binance")
-print(monero.get_data("2020-01-01", "2020-02-01", "1d"))
+print(monero.get_data("2020-01-01", "2020-02-01", "1d", "opens"))

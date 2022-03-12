f8e410193f7d1f23174e80882b3e47b627f535d5
JadonLee8 <jadonlee888@gmail.com>
2022-03-12 14:33:09 -0600

    Can now automatically create a set of states.

--- Main.py
+++ Main.py
@@ -1,20 +1,26 @@
 from ChartData import Crypto
 import ChartData
 from Markov import SimpleState
+from Markov import SimpleStageSet
 
+# currently this file is pretty much just for testing as I create methods for stuff.
 bitcoin = Crypto("btc", "Bitcoin", "binance")
-test_state = SimpleState(10, bitcoin, "2020-01-01", "2020-01-04", 3)
-print(test_state.state)
+# test_state = SimpleState(10, bitcoin, "2020-01-01", "2020-01-04", 3)
+# print(test_state.state)
+#
+# test_state_2 = SimpleState(10, bitcoin, "2020-01-01", "2020-01-04", 3)
+# print(test_state_2.is_similar(test_state, .05))
+#
+# test_state_3 = SimpleState(10, bitcoin, "2020-01-01", "2020-01-05", 4)
+# print(test_state_3.is_similar(test_state, .05))
+#
+# test_state_4 = SimpleState(10, bitcoin, "2019-09-27", "2019-09-30", 3)
+# test_state_5 = SimpleState(10, bitcoin, "2020-01-12", "2020-01-15", 3)
+# print(test_state_4.is_similar(test_state_5, .02))
 
-test_state_2 = SimpleState(10, bitcoin, "2020-01-01", "2020-01-04", 3)
-print(test_state_2.is_similar(test_state, .05))
-
-test_state_3 = SimpleState(10, bitcoin, "2020-01-01", "2020-01-05", 4)
-print(test_state_3.is_similar(test_state, .05))
-
-test_state_4 = SimpleState(10, bitcoin, "2019-09-27", "2019-09-30", 3)
-test_state_5 = SimpleState(10, bitcoin, "2020-01-12", "2020-01-15", 3)
-print(test_state_4.is_similar(test_state_5, .01))
+stage_set_test = SimpleStageSet("2017-11-12", "2020-11-27", 3, bitcoin, .03, 1111)
+stage_set_test.create_simple_states()
+print(stage_set_test.set)
 
 # print(bitcoin.get_data("2020-01-01", "2020-02-01", "1d", "opens"))
 #
--- Markov.py
+++ Markov.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 from ChartData import Crypto
 
 
@@ -33,7 +35,107 @@ class SimpleState:
             return False
 
 
+class SimpleStageSet:
+    def __init__(self, set_start, set_end, num_days_in_state, stonk, forgiveness, num_states):
+        self.set = []
+        self.num_days_in_state = num_days_in_state
+        self.set_start = set_start
+        self.set_end = set_end
+        self.stonk = stonk
+        self.forgiveness = forgiveness
+        self.num_states = num_states
+
+    def create_simple_states(self):
+        print("Creating a set of simple states from '" + self.set_start + "' to '" + self.set_end + "'")
+        # the below for loop is an incredibly inefficient way to calculate what the date will be for every increment so
+        # that you will know when the date has passed the end_date TODO: While writing the below method, there were
+        #  definitely some easy ways to optimize, definitely should make an effort to do
+        bulk = self.stonk.get_data(self.set_start, self.set_end, "1d", "opens")
+        temp_stage = []
+        for i in range(0, len(bulk) - 2):
+            for y in range(i, i + 3):
+                temp_stage.append(bulk[y])
+            self.set.append(temp_stage)
+            temp_stage = []
 
 
-
+# method made obsolete by new method with one large data request
+def calculate_date(start_date, days_in_advance):
+    year = start_date[0] + start_date[1] + start_date[2] + start_date[3]
+    day = start_date[8] + start_date[9]
+    month = start_date[5] + start_date[6]
+    num_year = int(year)
+    num_day = int(day)
+    num_month = int(month)
+    for i in range(0, days_in_advance-1):
+        if month == "01" or month == "03" or month == "05" or month == "07" or month == "08" or month == "10" or month == "12":
+            if day == "31":
+                if month == "12":
+                    num_year += 1
+                    year = str(year)
+                else:
+                    num_month += 1
+                    if num_month < 10:
+                        month = "0" + str(num_month)
+                    else:
+                        month = str(num_month)
+            else:
+                num_day += 1
+                if num_day < 10:
+                    day = "0" + str(num_day)
+                else:
+                    day = str(num_day)
+        elif month == "04" or month == "06" or month == "09" or month == "11":
+            if day == "30":
+                if month == "12":
+                    num_year += 1
+                    year = str(year)
+                else:
+                    num_month += 1
+                    if num_month < 10:
+                        month = "0" + str(num_month)
+                    else:
+                        month = str(num_month)
+            else:
+                num_day += 1
+                if num_day < 10:
+                    day = "0" + str(num_day)
+                else:
+                    day = str(num_day)
+        else:
+            if year == 2024 and year == 2020 and year == 2016 and year == 2012 and year == 2008 and year == 2004 and year == 2000:
+                if day == "29":
+                    if month == "12":
+                        num_year += 1
+                        year = str(year)
+                    else:
+                        num_month += 1
+                        if num_month < 10:
+                            month = "0" + str(num_month)
+                        else:
+                            month = str(num_month)
+                else:
+                    num_day += 1
+                    if num_day < 10:
+                        day = "0" + str(num_day)
+                    else:
+                        day = str(num_day)
+            else:
+                if day == "28":
+                    if month == "12":
+                        num_year += 1
+                        year = str(year)
+                    else:
+                        num_month += 1
+                        if num_month < 10:
+                            month = "0" + str(num_month)
+                        else:
+                            month = str(num_month)
+                else:
+                    num_day += 1
+                    if num_day < 10:
+                        day = "0" + str(num_day)
+                    else:
+                        day = str(num_day)
+    return year + "-" + month + "-" + day
 
